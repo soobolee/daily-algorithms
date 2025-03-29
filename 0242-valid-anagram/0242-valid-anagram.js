961243a01fa6eb25883c6a6394e6e80a8ca542f5
SooBo <sjstnqh@naver.com>
2025-03-29 22:49:35 +0900

    Time: 12 ms (75.8%), Space: 58.8 MB (22.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,8 +4,38 @@
  * @return {boolean}
  */
 const isAnagram = function(s, t) {
-	const sArray = s.split("").sort();
-	const tArray = t.split("").sort();
-	
-	return sArray.join("") === tArray.join("")
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const sHash = getCharAnagram(s);
+    const tHash = getCharAnagram(t);
+
+    for (const key of Object.keys(sHash)) {
+        if (!tHash[key]) {
+            return false;
+        }
+
+        if (sHash[key] !== tHash[key]) {
+            return false;
+        }
+    }
+
+    return true;
 };
+
+const getCharAnagram = function(string) {
+    const hash = {};
+    const charArray = string.split("");
+
+    for (const char of charArray) {
+        if (char in hash) {
+            let charCount = hash[char];
+            hash[char] = ++charCount;
+        } else {
+            hash[char] = 1;
+        }
+    }
+
+    return hash;
+}
