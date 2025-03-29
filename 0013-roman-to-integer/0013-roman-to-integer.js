72c29b2a5704f0cdeaada7920a7950299d858d22
SooBo <sjstnqh@naver.com>
2025-03-29 18:46:00 +0900

    Time: 2 ms (99.55%), Space: 59.9 MB (59.23%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -5,26 +5,23 @@
 var romanToInt = function(s) {
     let returnValue = 0;
 
-    const value = new Map()
-    value.set('I', 1);
-    value.set('V', 5);
-    value.set('X', 10);
-    value.set('L', 50);
-    value.set('C', 100);
-    value.set('D', 500);
-    value.set('M', 1000);
+    const strToInt = {
+    I: 1, V: 5, X: 10,
+    L: 50, C: 100,
+    D: 500, M: 1000
+    };
 
     const strArray = s.split("").reverse();
     let beforeValue = 0;
 
-    for (str of strArray) {
-        if (value.get(str) < beforeValue) {
-            returnValue -= value.get(str);
+    for (const str of strArray) {
+        if (strToInt[str] < beforeValue) {
+            returnValue -= strToInt[str];
         } else {
-            returnValue += value.get(str);
+            returnValue += strToInt[str];
         }
         
-        beforeValue = value.get(str);
+        beforeValue = strToInt[str];
     }
 
     return returnValue
