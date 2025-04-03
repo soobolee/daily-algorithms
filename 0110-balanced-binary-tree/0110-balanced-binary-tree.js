4802de9b4f463b4bde5f35cf455351eec80973cc
SooBo <sjstnqh@naver.com>
2025-04-03 20:36:52 +0900

    Time: 1 ms (89.42%), Space: 60.1 MB (50.5%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -14,33 +14,20 @@ var isBalanced = function(root) {
     if (!root) {
         return true;
     }
-    let leftCnt = 0;
-    let rightCnt = 0;
-    let nodeBalanced = true;
 
     function dfs(node) {
         if (!node) {
             return 0;
         }
         const left = dfs(node.left);
-        const right = dfs(node.right)
+        const right = dfs(node.right);
 
-        if (Math.abs(left - right) > 1) {
-            nodeBalanced = false;
+        if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+            return -1;
         }
 
         return Math.max(left, right) + 1;
     }
 
-    if (root.left) {
-        leftCnt = dfs(root.left);
-    }
-    if (root.right) {
-        rightCnt = dfs(root.right);
-    }
-
-    if (!nodeBalanced) {
-        return false;
-    }
-    return Math.abs(leftCnt - rightCnt) < 2;
+    return dfs(root) > -1;
 };
