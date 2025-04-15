9559263885919b1a5cf0e74a3904eaea3cc61006
SooBo <sjstnqh@naver.com>
2025-04-15 18:30:01 +0900

    Time: 0 ms (100%), Space: 54.1 MB (58.26%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    const bits = n.toString(2);
-
     let oneCount = 0;
-    Array.from(bits).forEach(num => {
-        if (num === "1") {
+    let num = n;
+
+    while (num > 0) {
+        if (num & 1) {
             oneCount++;
         }
-    })
+
+        num = num >> 1;
+    }
 
     return oneCount;
 };
