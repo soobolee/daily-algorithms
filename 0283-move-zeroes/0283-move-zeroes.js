812bd01224e419f651c892e82dd3ae51ed7623f6
SooBo <sjstnqh@naver.com>
2025-04-16 17:34:43 +0900

    Time: 16 ms (9.55%), Space: 60.2 MB (64.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,17 +3,16 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            let left = i;
-            for (let right = i + 1; right < nums.length; right++) {
+    for (let left = 0; left < nums.length; left++) {
+        if (nums[left] === 0) {
+            for (let right = left + 1; right < nums.length; right++) {
                 if (nums[right] === 0) {
                     continue;
                 }
                 const temp = nums[right];
                 nums[right] = nums[left];
                 nums[left] = temp;
-                left = right;
+                left++;
             }
         }
     }
