9cd55963ca776dda61f8da4d8e1d088e71e463a0
SooBo <sjstnqh@naver.com>
2025-04-20 23:10:08 +0900

    Time: 0 ms (100%), Space: 53.5 MB (42.92%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 1) {
+        return 1;
+    }
+    if (n === 2) {
+        return 2;
+    }
+
+    let first = 1;
+    let second = 2;
+
+    for (let i = 3; i <= n; i++) {
+        let temp = first + second;
+        first = second;
+        second = temp;
+    }
+
+    return second;
+};
