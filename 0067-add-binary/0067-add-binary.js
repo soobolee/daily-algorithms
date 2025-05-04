f7b87532be6853018c8936729f7a0412846fdfaa
SooBo <sjstnqh@naver.com>
2025-05-04 18:37:57 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,26 +4,9 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    const aArray = a.split("").reverse();
-    const bArray = b.split("").reverse();
-    const longLength = Math.max(aArray.length, bArray.length)
-    let result = "";
-    let carry = 0;
+    const numa = parseInt(a, 2);
+    const numb = parseInt(b, 2);
+    const sum = numa + numb;
 
-    for (let i = 0; i < longLength; i++) {
-        const numA = Number(aArray[i]) || 0;
-        const numB = Number(bArray[i]) || 0;
-
-        const addRes = numA + numB + carry;
-
-        if (addRes >= 2) {
-            carry = 1;
-            result = (addRes - 2) + result;
-        } else {
-            carry = 0;
-            result = addRes + result;
-        }
-    }
-
-    return carry > 0 ? "1" + result : result;
+    return sum.toString(2);
 };
