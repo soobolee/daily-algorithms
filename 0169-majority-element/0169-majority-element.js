64b18520cb61acfd302d0f2d9b7742bb07acc06e
SooBo <sjstnqh@naver.com>
2025-05-04 18:30:10 +0900

    Time: 8 ms (21.95%), Space: 58.9 MB (19.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,17 +3,16 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const count = {};
-    let max = 0;
-    let maxKey = nums[0];
+    const hash = {};
+    let max = nums[0];
 
     for (const num of nums) {
-        count[num] = (count[num] || 0) + 1;
-        if (count[num] > max) {
-            max = count[num];
-            maxKey = num;
+        hash[num] = (hash[num] || 0) + 1;
+
+        if (hash[max] < hash[num]) {
+            max = num;
         }
     }
 
-    return maxKey;
+    return max;
 };
