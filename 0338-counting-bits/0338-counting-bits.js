3375feea1d248fa283d7d0a410eff84eb87ef271
SooBo <sjstnqh@naver.com>
2025-05-08 20:34:33 +0900

    Time: 2 ms (75.38%), Space: 61.1 MB (68.65%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -3,20 +3,11 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-    const res = [];
+    const result = new Array(n + 1).fill(0);
 
-    for (let i = 0; i <= n; i++) {
-        const bit = i.toString(2);
-
-        let sum = 0;
-        Array.from(bit).forEach((num) => {
-            if (num === "1") {
-                sum += 1;
-            }
-        })
-
-        res.push(sum);
+    for (let i = 1; i <= n; i++) {
+        result[i] = result[i >> 1] + (i & 1);
     }
 
-    return res;
+    return result;
 };
