4e5c1bd0ff9458826b99e3a49b74ce4a2ace5b10
SooBo <sjstnqh@naver.com>
2025-05-17 23:56:21 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var mostFrequentEven = function(nums) {
+    const hash = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0) {
+            hash[nums[i]] = (hash[nums[i]] || 0) + 1;
+        }
+    }
+
+    let max = -1;
+    for (key of Object.keys(hash)) {
+        const maxValue = Number(hash[max]) || -1;
+        const keyValue = Number(hash[key]);
+
+        if (keyValue > maxValue) {
+            max = key;
+        } else if (keyValue === maxValue) {
+            max = key < max ? key : max;
+        }
+    }
+
+    return Number(max);
+};
